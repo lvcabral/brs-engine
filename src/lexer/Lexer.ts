lvcabral/brs-engine

commit a98fd7facafa15a686d223ccf6e6c6468a65adfb

fix(lex,parse,e2e): Allow 'then' as a property (#186)
The `then` keyword isn't _actually_ a keyword, in that it can be used as
a valid identifier.  But it's still reserved as far as I can tell?

fixes #182

src/parser/Parser.ts
@@ -482,7 +482,18 @@ export class Parser {
             let elseIfTokens: Token[] = [];
             let endIf: Token | undefined;
 
-            if (check(Lexeme.Then)) {
+            /**
+             * A simple wrapper around `check`, to make tests for a `then` identifier.
+             * As with many other words, "then" is a keyword but not reserved, so associative
+             * arrays can have properties called "then".  It's a valid identifier sometimes, so the
+             * parser has to take on the burden of understanding that I guess.
+             * @returns `true` if the next token is an identifier with text "then", otherwise `false`.
+             */
+            function checkThen() {
+                return check(Lexeme.Identifier) && peek().text === "then";
+            }
+
+            if (checkThen()) {
                 // `then` is optional after `if ...condition...`, so only advance to the next token if `then` is present
                 then = advance();
             }
@@ -510,7 +521,7 @@ export class Parser {
                 while (check(Lexeme.ElseIf)) {
                     elseIfTokens.push(advance());
                     let elseIfCondition = expression();
-                    if (check(Lexeme.Then)) {
+                    if (checkThen()) {
                         // `then` is optional after `else if ...condition...`, so only advance to the next token if `then` is present
                         advance();
                     }
@@ -558,7 +569,7 @@ export class Parser {
                 while(match(Lexeme.ElseIf)) {
                     let elseIf = previous();
                     let elseIfCondition = expression();
-                    if (check(Lexeme.Then)) {
+                    if (checkThen()) {
                         // `then` is optional after `else if ...condition...`, so only advance to the next token if `then` is present
                         advance();
                     }

test/parser/controlFlow/If.test.js
@@ -18,7 +18,7 @@ describe("parser if statements", () => {
                 token(Lexeme.Integer, "1", new Int32(1)),
                 token(Lexeme.Less, "<"),
                 token(Lexeme.Integer, "2", new Int32(2)),
-                token(Lexeme.Then, "then"),
+                identifier("then"),
                 identifier("foo"),
                 token(Lexeme.Equal, "="),
                 token(Lexeme.True, "true", BrsBoolean.True),
@@ -39,7 +39,7 @@ describe("parser if statements", () => {
                 token(Lexeme.Integer, "1", new Int32(1)),
                 token(Lexeme.Less, "<"),
                 token(Lexeme.Integer, "2", new Int32(2)),
-                token(Lexeme.Then, "then"),
+                identifier("then"),
                 identifier("foo"),
                 token(Lexeme.Equal, "="),
                 token(Lexeme.True, "true", BrsBoolean.True),
@@ -63,7 +63,7 @@ describe("parser if statements", () => {
                 token(Lexeme.Integer, "1", new Int32(1)),
                 token(Lexeme.Less, "<"),
                 token(Lexeme.Integer, "2", new Int32(2)),
-                token(Lexeme.Then, "then"),
+                identifier("then"),
                 identifier("foo"),
                 token(Lexeme.Equal, "="),
                 token(Lexeme.True, "true", BrsBoolean.True),
@@ -71,7 +71,7 @@ describe("parser if statements", () => {
                 token(Lexeme.Integer, "1", new Int32(1)),
                 token(Lexeme.Equal, "="),
                 token(Lexeme.Integer, "2", new Int32(2)),
-                token(Lexeme.Then, "then"),
+                identifier("then"),
                 identifier("same"),
                 token(Lexeme.Equal, "="),
                 token(Lexeme.True, "true", BrsBoolean.True),
@@ -127,7 +127,7 @@ describe("parser if statements", () => {
                 token(Lexeme.Integer, "1", new Int32(1)),
                 token(Lexeme.Less, "<"),
                 token(Lexeme.Integer, "2", new Int32(2)),
-                token(Lexeme.Then, "then"),
+                identifier("then"),
                 token(Lexeme.Newline, "\n"),
                 identifier("foo"),
                 token(Lexeme.Equal, "="),
@@ -153,7 +153,7 @@ describe("parser if statements", () => {
                 token(Lexeme.Integer, "1", new Int32(1)),
                 token(Lexeme.Less, "<"),
                 token(Lexeme.Integer, "2", new Int32(2)),
-                token(Lexeme.Then, "then"),
+                identifier("then"),
                 token(Lexeme.Newline, "\n"),
                 identifier("foo"),
                 token(Lexeme.Equal, "="),
@@ -185,7 +185,7 @@ describe("parser if statements", () => {
                 token(Lexeme.Integer, "1", new Int32(1)),
                 token(Lexeme.Less, "<"),
                 token(Lexeme.Integer, "2", new Int32(2)),
-                token(Lexeme.Then, "then"),
+                identifier("then"),
                 token(Lexeme.Newline, "\n"),
                 identifier("foo"),
                 token(Lexeme.Equal, "="),
@@ -195,7 +195,7 @@ describe("parser if statements", () => {
                 token(Lexeme.Integer, "1", new Int32(1)),
                 token(Lexeme.Greater, ">"),
                 token(Lexeme.Integer, "2", new Int32(2)),
-                token(Lexeme.Then, "then"),
+                identifier("then"),
                 token(Lexeme.Newline, "\n"),
                 identifier("foo"),
                 token(Lexeme.Equal, "="),

test/parser/controlFlow/__snapshots__/If.test.js.snap
@@ -608,7 +608,7 @@ Array [
       },
       "then": Object {
         "isReserved": true,
-        "kind": 84,
+        "kind": 28,
         "literal": undefined,
         "location": Object {
           "end": Object {
@@ -886,7 +886,7 @@ Array [
       },
       "then": Object {
         "isReserved": true,
-        "kind": 84,
+        "kind": 28,
         "literal": undefined,
         "location": Object {
           "end": Object {
@@ -1300,7 +1300,7 @@ Array [
       },
       "then": Object {
         "isReserved": true,
-        "kind": 84,
+        "kind": 28,
         "literal": undefined,
         "location": Object {
           "end": Object {
@@ -1791,7 +1791,7 @@ Array [
       },
       "then": Object {
         "isReserved": true,
-        "kind": 84,
+        "kind": 28,
         "literal": undefined,
         "location": Object {
           "end": Object {
@@ -2017,7 +2017,7 @@ Array [
       },
       "then": Object {
         "isReserved": true,
-        "kind": 84,
+        "kind": 28,
         "literal": undefined,
         "location": Object {
           "end": Object {
@@ -2362,7 +2362,7 @@ Array [
       },
       "then": Object {
         "isReserved": true,
-        "kind": 84,
+        "kind": 28,
         "literal": undefined,
         "location": Object {
           "end": Object {

test/interpreter/If.test.js
@@ -60,7 +60,7 @@ describe("interpreter if statements", () => {
             new Stmt.If(
                 {
                     if: token(Lexeme.If, "if"),
-                    then: token(Lexeme.Then, "then"),
+                    then: identifier("then"),
                     endIf: token(Lexeme.EndIf, "end if")
                 },
                 new Expr.Binary(
@@ -85,7 +85,7 @@ describe("interpreter if statements", () => {
             new Stmt.If(
                 {
                     if: token(Lexeme.If, "if"),
-                    then: token(Lexeme.Then, "then"),
+                    then: identifier("then"),
                     endIf: token(Lexeme.EndIf, "end if")
                 },
                 new Expr.Binary(
@@ -119,7 +119,7 @@ describe("interpreter if statements", () => {
             new Stmt.If(
                 {
                     if: token(Lexeme.If, "if"),
-                    then: token(Lexeme.Then, "then"),
+                    then: identifier("then"),
                     elseIfs: [ token(Lexeme.ElseIf, "else if"), token(Lexeme.ElseIf, "else if") ],
                     else: token(Lexeme.Else, "else"),
                     endIf: token(Lexeme.EndIf, "end if")
@@ -167,7 +167,7 @@ describe("interpreter if statements", () => {
             new Stmt.If(
                 {
                     if: token(Lexeme.If, "if"),
-                    then: token(Lexeme.Then, "then"),
+                    then: identifier("then"),
                     elseIfs: [ token(Lexeme.ElseIf, "else if"), token(Lexeme.ElseIf, "else if") ],
                     else: token(Lexeme.Else, "else"),
                     endIf: token(Lexeme.EndIf, "end if")

src/lexer/Lexeme.ts
@@ -101,7 +101,6 @@ export enum Lexeme {
     Step,
     Sub,
     Tab,
-    Then,
     To,
     True,
     Type,

src/lexer/ReservedWords.ts
@@ -94,7 +94,6 @@ export const KeyWords: {[key: string]: L} = {
     return: L.Return,
     step: L.Step,
     sub: L.Sub,
-    then: L.Then,
     to: L.To,
     true: L.True,
     while: L.While,

test/e2e/Syntax.test.js
@@ -134,7 +134,8 @@ describe("end to end syntax", () => {
                 allArgs(outputStreams.stdout.write).filter(arg => arg !== "\n")
             ).toEqual([
                 // note: associative array keys are sorted before iteration
-                "createobject", "in", "run", "stop"
+                "createobject", "in", "run", "stop", "then",
+                "promise-like resolved to 'foo'"
             ]);
         });
     });

test/e2e/resources/reserved-words.brs
@@ -3,10 +3,29 @@ sub main()
         createObject: true,
         in: true,
         stop: true,
-        run: true
+        run: true,
+        then: "useful for promises!"
     }
 
     for each word in hasReservedWords
         print word
     end for
+
+    immediatePromise("foo").then(sub(result)
+        print "promise-like resolved to '" + result + "'"
+    end sub)
 end sub
+
+' A simple promise-like function that immediately resolves to the provided value.
+' You probably don't want to use it in production.
+' @param {*} val the value this promise-like should immediately resolve to
+' @returns {AssociativeArray} an associative array contianing a `then` property, used to chain
+'                             promise-like constructs.
+function immediatePromise(val as dynamic) as object
+    return {
+        __result: val
+        then: sub(resolved as function)
+            resolved(m.__result)
+        end sub
+    }
+end function

commit c78124b17fa8cacf987b478021223b107d11e5c9

fix(parse): Allow mixed-case then (#188)
Commit a98fd7f (#186) caused a regression in the if-statement parsing
code in which mixed-case `then`s were no longer allowed.  This fixes
that!

fixes #187

src/lexer/Lexer.ts
@@ -604,7 +604,7 @@ export class Lexer {
             tokens.push({
                 kind: kind,
                 text: text,
-                isReserved: ReservedWords.has(text),
+                isReserved: ReservedWords.has(text.toLowerCase()),
                 literal: literal,
                 location: locationOf(text)
             });

src/parser/Parser.ts
@@ -424,7 +424,7 @@ export class Parser {
             let name = advance();
 
             let maybeIn = peek();
-            if (check(Lexeme.Identifier) && maybeIn.text && maybeIn.text.toLowerCase() === "in") {
+            if (check(Lexeme.Identifier) && maybeIn.text.toLowerCase() === "in") {
                 advance();
             } else {
                 return addError(
@@ -490,7 +490,7 @@ export class Parser {
              * @returns `true` if the next token is an identifier with text "then", otherwise `false`.
              */
             function checkThen() {
-                return check(Lexeme.Identifier) && peek().text === "then";
+                return check(Lexeme.Identifier) && peek().text.toLowerCase() === "then";
             }
 
             if (checkThen()) {

test/parser/ParserTests.js
@@ -14,7 +14,9 @@ exports.token = function(kind, text, literal) {
     return {
         kind: kind,
         text: text,
-        isReserved: brs.lexer.ReservedWords.has(text),
+        isReserved: brs.lexer.ReservedWords.has(
+            (text || "").toLowerCase()
+        ),
         literal: literal,
         location: {
             start: { line: -9, column: -9 },

test/parser/controlFlow/If.test.js
@@ -39,7 +39,7 @@ describe("parser if statements", () => {
                 token(Lexeme.Integer, "1", new Int32(1)),
                 token(Lexeme.Less, "<"),
                 token(Lexeme.Integer, "2", new Int32(2)),
-                identifier("then"),
+                identifier("Then"),
                 identifier("foo"),
                 token(Lexeme.Equal, "="),
                 token(Lexeme.True, "true", BrsBoolean.True),
@@ -127,7 +127,7 @@ describe("parser if statements", () => {
                 token(Lexeme.Integer, "1", new Int32(1)),
                 token(Lexeme.Less, "<"),
                 token(Lexeme.Integer, "2", new Int32(2)),
-                identifier("then"),
+                identifier("THEN"),
                 token(Lexeme.Newline, "\n"),
                 identifier("foo"),
                 token(Lexeme.Equal, "="),

test/parser/controlFlow/__snapshots__/If.test.js.snap
@@ -620,7 +620,7 @@ Array [
             "line": -9,
           },
         },
-        "text": "then",
+        "text": "THEN",
       },
     },
   },
@@ -2029,7 +2029,7 @@ Array [
             "line": -9,
           },
         },
-        "text": "then",
+        "text": "Then",
       },
     },
   },