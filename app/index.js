e8bda54e47e7cf50b714e41de2502a745729e39f
Marcelo Lv Cabral <lvcabral@hotmail.com>
2019-09-22 00:09:30 -0700

    Added option to configure device font, changed default to "Roboto"

--- app/index.js
+++ app/index.js
@@ -47,6 +47,7 @@ const deviceData = {
     locale: "en_US",
     clockFormat: "12h",
     displayMode: "720p", // Only this mode is supported for now
+    defaultFont: "Roboto", // Options: Roboto or Open Sans
 };
 
 // Load Registry
