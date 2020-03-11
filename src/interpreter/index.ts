a7048578438f27b914a205a2ab5249b1030e075e
Marcelo Lv Cabral <marcelo@lvcabral.com>
2020-03-10 17:35:36 -0700

    fix(interp): Automatically box return values for 'as object' return types (#360)
    RBI will automatically box an intrinsic value when it's returned from a
    function that declares an `as object` return type.  This makes the
    function behave quite similarly to an `as dynamic` function, in that it
    can seemingly return any value.  The only difference is that
    `as dynamic` won't automatically box something like `return false` into
    an `roBoolean`, while the `as object` variant will!
    
    fixes #359

--- src/interpreter/index.ts
+++ src/interpreter/index.ts
@@ -977,6 +977,14 @@ export class Interpreter implements Expr.Visitor<BrsType>, Stmt.Visitor<BrsType>
 
                 if (
                     returnedValue &&
+                    isBoxable(returnedValue) &&
+                    satisfiedSignature.signature.returns === ValueKind.Object
+                ) {
+                    returnedValue = returnedValue.box();
+                }
+
+                if (
+                    returnedValue &&
                     satisfiedSignature.signature.returns !== ValueKind.Dynamic &&
                     satisfiedSignature.signature.returns !== returnedValue.kind
                 ) {
