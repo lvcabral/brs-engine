5e6ebce78450986fb73087d68d6f0c555c542587
Marcelo Lv Cabral <marcelo@lvcabral.com>
2020-03-10 19:12:45 -0700

    feat(stdlib): Implement getString() and toStr() on RoString (#362)
    I forgot about these, whoops!
    
    fixes #361

--- src/brsTypes/components/RoString.ts
+++ src/brsTypes/components/RoString.ts
@@ -30,10 +30,7 @@ export class RoString extends BrsComponent implements BrsValue, Comparable, Unbo
 
         this.intrinsic = initialValue;
         this.registerMethods({
-            ifString: [
-                this.setString,
-                // TODO: getString
-            ],
+            ifString: [this.setString, this.getString],
             ifStringOps: [
                 this.appendString,
                 this.len,
@@ -55,7 +52,7 @@ export class RoString extends BrsComponent implements BrsValue, Comparable, Unbo
                 this.encodeUriComponent,
                 this.decodeUriComponent,
             ],
-            // TODO: ifToStr and it's toStr() method
+            ifToStr: [this.toStr],
         });
     }
 
@@ -121,6 +118,14 @@ export class RoString extends BrsComponent implements BrsValue, Comparable, Unbo
             }
             return BrsInvalid.Instance;
         },
+    });
+
+    private getString = new Callable("GetString", {
+        signature: {
+            args: [],
+            returns: ValueKind.String,
+        },
+        impl: _interpreter => this.intrinsic,
     });
 
     /** Appends the first len characters of s to the end of the string. */
@@ -459,5 +464,13 @@ export class RoString extends BrsComponent implements BrsValue, Comparable, Unbo
         impl: _interpreter => {
             return new BrsString(decodeURIComponent(this.intrinsic.value));
         },
+    });
+
+    private toStr = new Callable("toStr", {
+        signature: {
+            args: [],
+            returns: ValueKind.String,
+        },
+        impl: _interpreter => this.intrinsic,
     });
 }
