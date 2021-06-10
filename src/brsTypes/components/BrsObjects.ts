sjbarag/brs

fix(rsg): Adds optional arg to RoString constructor (#533)
* fix(rsg): Adds optional arg to RoString constructor

* Changed instanceOf for a simple truthy check

Co-authored-by: Sean Barag <sean@barag.org>

* Fixed prettier task

Co-authored-by: Sean Barag <sean@barag.org>

src/brsTypes/components/BrsObjects.ts
@@ -32,7 +32,7 @@ export const BrsObjects = new Map<string, Function>([
         "roregex",
         (_: Interpreter, expression: BrsString, flags: BrsString) => new RoRegex(expression, flags),
     ],
-    ["rostring", (_: Interpreter, literal: BrsString) => new RoString(literal)],
+    ["rostring", (_: Interpreter) => new RoString()],
     ["roboolean", (_: Interpreter, literal: BrsBoolean) => new roBoolean(literal)],
     ["rodouble", (_: Interpreter, literal: Double) => new roDouble(literal)],
     ["rofloat", (_: Interpreter, literal: Float) => new roFloat(literal)],

lvcabral/brs-engine

src/brsTypes/components/BrsObjects.ts
@@ -51,7 +51,7 @@ export const BrsObjects = new Map<string, Function>([
         (interpreter: Interpreter, expression: BrsString, flags: BrsString) =>
             new RoRegex(expression, flags),
     ],
-    ["rostring", (interpreter: Interpreter, literal: BrsString) => new RoString(literal)],
+    ["rostring", (interpreter: Interpreter) => new RoString()],
     ["roboolean", (interpreter: Interpreter, literal: BrsBoolean) => new roBoolean(literal)],
     ["rodouble", (interpreter: Interpreter, literal: Double) => new roDouble(literal)],
     ["rofloat", (interpreter: Interpreter, literal: Float) => new roFloat(literal)],