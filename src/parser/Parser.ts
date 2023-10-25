c39414ee3e056a9c377cdd0bb451a2dff5c0f16b
Nadiia Padalka <35704032+nadiapadalka@users.noreply.github.com>
2023-10-25 14:09:54 +0300

    add logic for optional chaining (#21)
    * add logic for optional chaining
    
    * remove console log
    
    * format doc
    
    * fix test
    
    * fix uts

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -545,6 +545,7 @@ export class Parser {
                     Lexeme.Newline,
                     Lexeme.Colon,
                     Lexeme.Eof,
+                    Lexeme.Identifier,
                     ...additionalterminators
                 );
             }
@@ -1442,6 +1443,8 @@ export class Parser {
             while (true) {
                 if (match(Lexeme.LeftParen)) {
                     expr = finishCall(expr);
+                } else if (match(Lexeme.Print)) {
+                    // doing nothing as invalid check was before
                 } else if (match(Lexeme.LeftSquare)) {
                     indexedGet();
                 } else if (match(Lexeme.AtSymbol)) {
