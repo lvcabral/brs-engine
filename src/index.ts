7331046d46ff12e91a6a46933dd7fe9523fab5a9
Marcelo Lv Cabral <marcelo@lvcabral.com>
2023-11-05 20:01:30 -0700

    Fixed formatting

--- src/index.ts
+++ src/index.ts
@@ -182,7 +182,7 @@ export function executeLine(contents: string, interpreter: Interpreter) {
             });
         }
     } catch (e: any) {
-        if(!(e instanceof BrsError.BrsError)) {
+        if (!(e instanceof BrsError.BrsError)) {
             console.error("Interpreter execution error: ", e.message);
         }
         return;
