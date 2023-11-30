bee573c1dbc412ed4e566f96bca2927048fb3a46
Marcelo Lv Cabral <marcelo@lvcabral.com>
2023-11-30 16:21:31 -0700

    Implemented `roAppMemoryMonitor` and `roDeviceInfo.enableLowGeneralMemoryEvent`

---
+++ src/brsTypes/components/RoAppMemoryMonitor.ts
@@ -0,0 +1,103 @@
+import { BrsValue, ValueKind, BrsInvalid, BrsBoolean } from "../BrsType";
+import { BrsComponent } from "./BrsComponent";
+import { BrsType, RoMessagePort } from "..";
+import { Callable, StdlibArgument } from "../Callable";
+import { Interpreter } from "../../interpreter";
+import { Int32 } from "../Int32";
+
+export class RoAppMemoryMonitor extends BrsComponent implements BrsValue {
+    private id: number;
+    private order: BrsType[];
+    private credData: string;
+    private port?: RoMessagePort;
+    readonly kind = ValueKind.Object;
+
+    constructor() {
+        super("roAppMemoryMonitor");
+        this.id = Math.floor(Math.random() * 100) + 1;
+        this.order = [];
+        this.credData = "";
+        this.registerMethods({
+            ifAppMemoryMonitor: [
+                this.enableMemoryWarningEvent,
+                this.getMemoryLimitPercent,
+                this.getChannelAvailableMemory,
+            ],
+            ifSetMessagePort: [this.setMessagePort],
+            ifGetMessagePort: [this.getMessagePort],
+        });
+    }
+
+    toString(parent?: BrsType): string {
+        return "<Component: roAppMemoryMonitor>";
+    }
+
+    equalTo(other: BrsType) {
+        return BrsBoolean.False;
+    }
+
+    // ifChannelStore ------------------------------------------------------------------------------------
+
+    /** Enables a channel to be alerted when it has reached 80% of its memory usage limit. */
+    private enableMemoryWarningEvent = new Callable("enableMemoryWarningEvent", {
+        signature: {
+            args: [new StdlibArgument("enable", ValueKind.Boolean)],
+            returns: ValueKind.Boolean,
+        },
+        impl: (_: Interpreter, enable: BrsBoolean) => {
+            // Mocked until roAppMemoryMonitorEvent is implemented
+            return enable;
+        },
+    });
+
+    /** Returns the usage percentage of memory limit for the channel. */
+    private getMemoryLimitPercent = new Callable("getMemoryLimitPercent", {
+        signature: {
+            args: [],
+            returns: ValueKind.Int32,
+        },
+        impl: (_: Interpreter) => {
+            // Returning 5% until proper memory management is implemented
+            return new Int32(5);
+        },
+    });
+
+    /** Returns the estimated kilobytes (Kb) of memory available for the channel. */
+    private getChannelAvailableMemory = new Callable("getChannelAvailableMemory", {
+        signature: {
+            args: [],
+            returns: ValueKind.Int32,
+        },
+        impl: (_: Interpreter) => {
+            // Returning 5% until proper memory management is implemented
+            return new Int32(5);
+        },
+    });
+
+    // ifGetMessagePort ----------------------------------------------------------------------------------
+
+    /** Returns the message port (if any) currently associated with the object */
+    private getMessagePort = new Callable("getMessagePort", {
+        signature: {
+            args: [],
+            returns: ValueKind.Object,
+        },
+        impl: (_: Interpreter) => {
+            return this.port === undefined ? BrsInvalid.Instance : this.port;
+        },
+    });
+
+    // ifSetMessagePort ----------------------------------------------------------------------------------
+
+    /** Sets the roMessagePort to be used for all events from the screen */
+    private setMessagePort = new Callable("setMessagePort", {
+        signature: {
+            args: [new StdlibArgument("port", ValueKind.Dynamic)],
+            returns: ValueKind.Void,
+        },
+        impl: (_: Interpreter, port: RoMessagePort) => {
+            this.port = port;
+            return BrsInvalid.Instance;
+        },
+    });
+}
