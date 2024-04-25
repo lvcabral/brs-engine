91abd239e8cb690c291d1e6461a9e1c00d220077
Marcelo Lv Cabral <marcelo@lvcabral.com>
2024-04-24 20:13:49 -0700

    Fixed roDateTime.getTimeZoneOffset()

--- src/worker/brsTypes/components/RoDateTime.ts
+++ src/worker/brsTypes/components/RoDateTime.ts
@@ -347,7 +347,7 @@ export class RoDateTime extends BrsComponent implements BrsValue {
                 const tzDate = new Date(date.toLocaleString("en-US", { timeZone: timeZone }));
                 return (tzDate.getTime() - utcDate.getTime()) / 6e4;
             };
-            return new Int32(-getOffset(interpreter.deviceInfo.get("timeZone")));
+            return new Int32(getOffset(interpreter.deviceInfo.get("timeZone"))* -1);
         },
     });
 
