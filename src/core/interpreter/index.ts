3252c1ee58998d6d97dab74f9edb99d96b66bd54
Marcelo Lv Cabral <marcelo@lvcabral.com>
2025-02-22 11:34:02 -0700

    Removed unused reference

--- src/core/interpreter/index.ts
+++ src/core/interpreter/index.ts
@@ -57,7 +57,7 @@ import { Scope, Environment, NotFound } from "./Environment";
 import { toCallable } from "./BrsFunction";
 import { BlockEnd, GotoLabel } from "../parser/Statement";
 import { runDebugger } from "./MicroDebugger";
-import { DataType, DebugCommand, defaultDeviceInfo, numberToHex, parseTextFile } from "../common";
+import { DataType, DebugCommand, numberToHex, parseTextFile } from "../common";
 /// #if !BROWSER
 import * as v8 from "v8";
 /// #endif
