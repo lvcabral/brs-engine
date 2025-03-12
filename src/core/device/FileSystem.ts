872aefb38631b6f4d02643e8d5e4c5a8f04427ea
Marcelo Lv Cabral <marcelo@lvcabral.com>
2025-03-11 17:13:56 -0700

    Updating forked dependencies (#495)
    * Updated `sprintf` module reference
    
    * Updated `upng` module reference
    
    * Updated `libweb` module reference
    
    * Updated `memory-fs` module reference
    
    * Updated reference for `zip`

--- src/core/device/FileSystem.ts
+++ src/core/device/FileSystem.ts
@@ -1,8 +1,8 @@
-import MemoryFileSystem from "memory-fs";
 import * as path from "path";
 import * as zenFS from "@zenfs/core";
 import * as nodeFS from "fs";
 import { Zip } from "@lvcabral/zip";
+import MemoryFileSystem from "@lvcabral/memory-fs";
 
 /** Proxy Object to make File System volumes case insensitive, same as Roku devices */
 
