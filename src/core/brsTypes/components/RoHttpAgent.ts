577b51cadd5f0f7e2dafc33a86ed5dfb060354b9
Marcelo Lv Cabral <marcelo@lvcabral.com>
2025-03-19 12:32:07 -0700

    Implemented `roHttpAgent` component (#499)

---
+++ src/core/brsTypes/components/RoHttpAgent.ts
@@ -0,0 +1,38 @@
+import { BrsValue, ValueKind, BrsBoolean } from "../BrsType";
+import { BrsComponent } from "./BrsComponent";
+import { BrsType } from "..";
+import { BrsHttpAgent, IfHttpAgent } from "../interfaces/IfHttpAgent";
+
+export class RoHttpAgent extends BrsComponent implements BrsValue, BrsHttpAgent {
+    readonly kind = ValueKind.Object;
+    readonly customHeaders: Map<string, string>;
+    cookiesEnabled: boolean;
+
+    constructor() {
+        super("roHttpAgent");
+        this.cookiesEnabled = false;
+        this.customHeaders = new Map<string, string>();
+        const ifHttpAgent = new IfHttpAgent(this);
+        this.registerMethods({
+            ifHttpAgent: [
+                ifHttpAgent.addHeader,
+                ifHttpAgent.setHeaders,
+                ifHttpAgent.initClientCertificates,
+                ifHttpAgent.setCertificatesFile,
+                ifHttpAgent.setCertificatesDepth,
+                ifHttpAgent.enableCookies,
+                ifHttpAgent.getCookies,
+                ifHttpAgent.addCookies,
+                ifHttpAgent.clearCookies,
+            ],
+        });
+    }
+
+    toString(parent?: BrsType): string {
+        return "<Component: roHttpAgent>";
+    }
+
+    equalTo(other: BrsType) {
+        return BrsBoolean.False;
+    }
+}
