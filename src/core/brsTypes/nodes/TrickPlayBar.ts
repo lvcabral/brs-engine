253e19d529011c0405b8b4fb858a65cc6f685f72
Marcelo Lv Cabral <marcelo@lvcabral.com>
2025-06-03 00:33:55 -0700

    Implemented support for state icons on `TrickPlayBar`

--- src/core/brsTypes/nodes/TrickPlayBar.ts
+++ src/core/brsTypes/nodes/TrickPlayBar.ts
@@ -1,5 +1,5 @@
 import { FieldKind, FieldModel } from "./Field";
-import { AAMember, BrsString, Float, Label, Poster, BrsBoolean, Int32, BrsType, isBrsString } from "..";
+import { AAMember, BrsString, Float, Label, Poster, BrsBoolean, Int32, BrsType, isBrsString, RoBitmap } from "..";
 import { Group } from "./Group";
 import { Interpreter } from "../../interpreter";
 import { IfDraw2D } from "../interfaces/IfDraw2D";
@@ -22,6 +22,9 @@ export class TrickPlayBar extends Group {
     private readonly barTicker: Poster;
     private readonly position: Label;
     private readonly remaining: Label;
+    private readonly bmpIcons: Map<string, RoBitmap>;
+    private stateIcon: RoBitmap | undefined;
+    private stateIconTimeout: number = -1;
 
     constructor(initializedFields: AAMember[] = [], readonly name: string = "TrickPlayBar") {
         super([], name);
@@ -52,6 +55,18 @@ export class TrickPlayBar extends Group {
         this.linkField(this.barProgress, "blendColor", "filledBarBlendColor");
         this.linkField(this.barProgress, "uri", "filledBarImageUri");
         this.linkField(this.barTicker, "blendColor", "currentTimeMarkerBlendColor");
+        this.setFieldValue("width", new Int32(this.barW));
+        this.bmpIcons = new Map<string, RoBitmap>([
+            ["skip-right", this.loadBitmap(`common:/images/${this.resolution}/icon_skipAhead.png`)!],
+            ["skip-left", this.loadBitmap(`common:/images/${this.resolution}/icon_skipBack.png`)!],
+            ["replay", this.loadBitmap(`common:/images/${this.resolution}/icon_replay.png`)!],
+            ["ff-x1", this.loadBitmap(`common:/images/${this.resolution}/icon_FFx1.png`)!],
+            ["ff-x2", this.loadBitmap(`common:/images/${this.resolution}/icon_FFx2.png`)!],
+            ["ff-x3", this.loadBitmap(`common:/images/${this.resolution}/icon_FFx3.png`)!],
+            ["rw-x1", this.loadBitmap(`common:/images/${this.resolution}/icon_RWx1.png`)!],
+            ["rw-x2", this.loadBitmap(`common:/images/${this.resolution}/icon_RWx2.png`)!],
+            ["rw-x3", this.loadBitmap(`common:/images/${this.resolution}/icon_RWx3.png`)!],
+        ]);
     }
 
     set(index: BrsType, value: BrsType, alwaysNotify: boolean = false, kind?: FieldKind) {
@@ -89,6 +104,18 @@ export class TrickPlayBar extends Group {
         }
     }
 
+    setStateIcon(iconName: string, timeout: number = -1) {
+        // Timeout of 0 means no icon, -1 means no timeout
+        if (timeout === 0) {
+            this.stateIcon = undefined;
+            this.stateIconTimeout = 0;
+        }
+        this.stateIcon = this.bmpIcons.get(iconName);
+        if (this.stateIcon) {
+            this.stateIconTimeout = timeout > 0 ? Date.now() + timeout : -1;
+        }
+    }
+
     renderNode(interpreter: Interpreter, origin: number[], angle: number, opacity: number, draw2D?: IfDraw2D) {
         if (!this.isVisible()) {
             return;
@@ -102,6 +129,15 @@ export class TrickPlayBar extends Group {
         opacity = opacity * this.getOpacity();
         this.updateBoundingRects(rect, origin, angle);
         this.renderChildren(interpreter, drawTrans, angle, opacity, draw2D);
+        if (this.stateIcon && (this.stateIconTimeout === -1 || this.stateIconTimeout > Date.now())) {
+            const iconRect = {
+                x: rect.x + (size.width - this.stateIcon.width) / 2,
+                y: rect.y + this.barH,
+                width: this.stateIcon.width,
+                height: this.stateIcon.height,
+            };
+            this.drawImage(this.stateIcon, iconRect, angle, opacity, draw2D);
+        }
         this.updateParentRects(origin, angle);
     }
 }
