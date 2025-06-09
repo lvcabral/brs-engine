74c4b50de5b2be307aa1a527cb1e330db771e2fe
Marcelo Lv Cabral <marcelo@lvcabral.com>
2025-06-08 23:17:40 -0700

    Reduced complexity of `set()`

--- src/core/brsTypes/nodes/SoundEffect.ts
+++ src/core/brsTypes/nodes/SoundEffect.ts
@@ -32,9 +32,7 @@ export class SoundEffect extends RoSGNode {
         if (!isBrsString(index)) {
             throw new Error("RoSGNode indexes must be strings");
         }
-
         const fieldName = index.getValue().toLowerCase();
-
         if (fieldName === "control" && isBrsString(value)) {
             const validControl = ["play", "stop"];
             const control = value.getValue().toLowerCase();
@@ -43,18 +41,7 @@ export class SoundEffect extends RoSGNode {
                 return super.set(index, value, alwaysNotify, kind);
             }
             if (control === "play") {
-                const volume = this.getFieldValueJS("volume") as number;
-                this.stream = BrsDevice.getSfxStream(this.audioId);
-                if (this.stream === -1) {
-                    if (BrsDevice.isDevMode) {
-                        BrsDevice.stderr.write(`warning,No available sound effect streams for ${this.uri}`);
-                    }
-                    this.setState(MediaEvent.TOO_MANY);
-                } else {
-                    rootObjects.sfx[this.stream] = this;
-                    postMessage(`sfx,trigger,${this.uri},${volume},${this.stream}`);
-                    this.setState(MediaEvent.START_PLAY);
-                }
+                this.play();
             } else if (control === "stop") {
                 this.stop();
             }
@@ -63,26 +50,7 @@ export class SoundEffect extends RoSGNode {
                 this.stop();
             }
             const uri = value.getValue().toLowerCase();
-            if ((uri.startsWith("pkg:") && BrsDevice.fileSystem.existsSync(uri)) || uri.startsWith("http")) {
-                this.uri = uri;
-                const sfxIndex = BrsDevice.sfx.findIndex((wav) => wav === uri);
-                if (sfxIndex > -1) {
-                    this.audioId = sfxIndex;
-                } else {
-                    this.audioId = BrsDevice.sfx.length;
-                    postMessage(`sfx,new,${uri},${this.audioId}`);
-                    BrsDevice.sfx.push(uri);
-                }
-                super.set(new BrsString("loadStatus"), new BrsString("ready"));
-            } else {
-                if (BrsDevice.isDevMode) {
-                    BrsDevice.stderr.write(`warning,Invalid sound effect URI: ${uri}`);
-                }
-                this.uri = "";
-                this.audioId = -1;
-                super.set(new BrsString("loadStatus"), new BrsString("failed"));
-                this.setState(MediaEvent.FAILED);
-            }
+            this.setUri(uri);
         } else if (fieldName === "volume" && isBrsNumber(value)) {
             const volume = value.getValue() as number;
             if (volume < 0 || volume > 100) {
@@ -125,6 +93,44 @@ export class SoundEffect extends RoSGNode {
                 break;
         }
         super.set(new BrsString("state"), new BrsString(this.state));
+    }
+
+    private setUri(uri: string) {
+        if ((uri.startsWith("pkg:") && BrsDevice.fileSystem.existsSync(uri)) || uri.startsWith("http")) {
+            this.uri = uri;
+            const sfxIndex = BrsDevice.sfx.findIndex((wav) => wav === uri);
+            if (sfxIndex > -1) {
+                this.audioId = sfxIndex;
+            } else {
+                this.audioId = BrsDevice.sfx.length;
+                postMessage(`sfx,new,${uri},${this.audioId}`);
+                BrsDevice.sfx.push(uri);
+            }
+            super.set(new BrsString("loadStatus"), new BrsString("ready"));
+        } else {
+            if (BrsDevice.isDevMode) {
+                BrsDevice.stderr.write(`warning,Invalid sound effect URI: ${uri}`);
+            }
+            this.uri = "";
+            this.audioId = -1;
+            super.set(new BrsString("loadStatus"), new BrsString("failed"));
+            this.setState(MediaEvent.FAILED);
+        }
+    }
+
+    private play() {
+        const volume = this.getFieldValueJS("volume") as number;
+        this.stream = BrsDevice.getSfxStream(this.audioId);
+        if (this.stream === -1) {
+            if (BrsDevice.isDevMode) {
+                BrsDevice.stderr.write(`warning,No available sound effect streams for ${this.uri}`);
+            }
+            this.setState(MediaEvent.TOO_MANY);
+        } else {
+            rootObjects.sfx[this.stream] = this;
+            postMessage(`sfx,trigger,${this.uri},${volume},${this.stream}`);
+            this.setState(MediaEvent.START_PLAY);
+        }
     }
 
     private stop() {
