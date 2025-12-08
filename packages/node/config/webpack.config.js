a87e982c37d541666dc33dc09d65960c18759f40
Marcelo Lv Cabral <marcelo@lvcabral.com>
2025-12-08 09:50:37 -0700

    Fixed code formatting

--- packages/node/config/webpack.config.js
+++ packages/node/config/webpack.config.js
@@ -104,9 +104,7 @@ module.exports = (env) => {
                 ],
             },
             resolve: sharedResolve,
-            plugins: [
-                new ShebangPlugin(),
-            ],
+            plugins: [new ShebangPlugin()],
             externals: {
                 "./brs.node.js": "commonjs ./brs.node.js",
                 "brs-engine": "commonjs ./brs.node.js",
