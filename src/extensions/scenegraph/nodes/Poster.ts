75e5856f862ef99b64526b052a3cd049477d7c8d
Marcelo Lv Cabral <marcelo@lvcabral.com>
2025-12-30 16:45:32 -0700

    Improved if logic

--- src/extensions/scenegraph/nodes/Poster.ts
+++ src/extensions/scenegraph/nodes/Poster.ts
@@ -119,18 +119,16 @@ export class Poster extends Group {
     private updateRect(rect: Rect) {
         const bitmapHeight = this.bitmap?.height ?? 0;
         const bitmapWidth = this.bitmap?.width ?? 0;
+        // Roku scales the Poster bitmap based on the current display mode
         if (this.resolution === BrsDevice.getDisplayMode()) {
             rect.height = rect.height <= 0 ? bitmapHeight : rect.height;
             rect.width = rect.width <= 0 ? bitmapWidth : rect.width;
+        } else if (this.resolution === "FHD") {
+            rect.height = rect.height <= 0 ? bitmapHeight * 1.5 : rect.height;
+            rect.width = rect.width <= 0 ? bitmapWidth * 1.5 : rect.width;
         } else {
-            // Roku scales the Poster bitmap based on the current display mode
-            if (this.resolution === "FHD") {
-                rect.height = rect.height <= 0 ? bitmapHeight * 1.5 : rect.height;
-                rect.width = rect.width <= 0 ? bitmapWidth * 1.5 : rect.width;
-            } else {
-                rect.height = rect.height <= 0 ? bitmapHeight / 1.5 : rect.height;
-                rect.width = rect.width <= 0 ? bitmapWidth / 1.5 : rect.width;
-            }
+            rect.height = rect.height <= 0 ? bitmapHeight / 1.5 : rect.height;
+            rect.width = rect.width <= 0 ? bitmapWidth / 1.5 : rect.width;
         }
     }
 
