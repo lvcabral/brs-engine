9206e3a0faaf65e6d14eecf6d7142b51aa75bc53
Marcelo Lv Cabral <marcelo@lvcabral.com>
2026-01-24 00:11:48 -0700

    Better error message

--- src/core/interpreter/index.ts
+++ src/core/interpreter/index.ts
@@ -584,7 +584,7 @@ export class Interpreter implements Expr.Visitor<BrsType>, Stmt.Visitor<BrsType>
         } else if (statement.name.text.toLowerCase() === "global") {
             this.addError(
                 new TypeMismatch({
-                    message: `Reserved name 'global' cannot be used in this context. Unable to cast `,
+                    message: `Reserved name 'global' cannot be used as a variable name. Unable to cast `,
                     left: {
                         type: ValueKind.Interface,
                         location: statement.name.location,
